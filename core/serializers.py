63edad50ee64af81e52e8306a8921819d1750e33
maha-j <73794370+maha-j@users.noreply.github.com>
2026-02-20 13:46:13 +0100

    Add serializers.py for DRF serializers

---
+++ core/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import User, Category, Course, Lesson
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
