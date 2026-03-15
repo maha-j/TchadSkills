9bbb4dd4bc0da981b55aa67defca2a8d6e6210b8
maha-j <73794370+maha-j@users.noreply.github.com>
2026-03-15 12:56:06 +0100

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -1,20 +1,47 @@
 from rest_framework import serializers
 from .models import User, Category, Course, Lesson
 
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, min_length=8)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'user_type', 'phone', 'avatar', 'bio')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name',
+                  'user_type', 'phone', 'avatar', 'bio', 'password')
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)  # Hash le mot de passe correctement
+        else:
+            user.set_unusable_password()
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
 
 class CourseSerializer(serializers.ModelSerializer):
     instructor_name = serializers.ReadOnlyField(source='instructor.get_full_name')
